aec5c8d8c72362382e26243cd69389aa59334820
vasundharaswamy19 <vasundharaswamy1908@gmail.com>
2023-11-19 18:57:18 -0800

    first commit

---
+++ src/components/Character.js
@@ -0,0 +1,5 @@
+function Character(){
+    return <h3>Character</h3>
+};
+
+export default Character;
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import Character from './Character';
+function List() {
+    return ( 
+        <div>
+     <h2>Characters</h2>
+     <Character/>
+ </div>
+    
+    );
+  }
+  export default List;
